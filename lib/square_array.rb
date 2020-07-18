517292157e2f94c9147f66a467372f212ae3412b
Sayana <nisayana23@yandex.ru>
2020-07-18 20:37:10 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
+# require 'pry'
 def square_array(array)
   # your code here
+  new_arr = Array.new
+  counter = 0
+  while array[counter] do
+    puts new_arr.push(array[counter]**2)
+    counter+=1
+  end
+  return new_arr
 end
