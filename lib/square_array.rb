fb52825fe1128eaf3d7ee427877c2daa378e39f5
Sayana <nisayana23@yandex.ru>
2020-07-18 20:30:35 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,9 @@
-require 'pry'
+# require 'pry'
 def square_array(array)
   # your code here
   counter = 0
   while array[counter] do
     puts array[counter]**2
-    binding.pry
     counter+=1
   end
 end
